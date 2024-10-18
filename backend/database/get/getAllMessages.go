397c20999d35cdf27a55b74f2e3e0e7e05a51957
MikeNike <kooduser@pupil.kood.tech>
2024-10-18 12:30:06 +0300

    vanad sõnumid saame kätte

---
+++ backend/database/get/getAllMessages.go
@@ -0,0 +1,43 @@
+package get
+
+import (
+	"fmt"
+
+	"main.go/backend/database/create"
+	"main.go/backend/helpers"
+	"main.go/backend/structs"
+)
+
+func GetAllMessages(username, ToUser string) ([]structs.SMessage, error) {
+	db, err := create.ConnectDB()
+	helpers.CheckError(err)
+	defer db.Close()
+	var msg structs.SMessage
+	fmt.Printf("my username: %v, the other username: %v\n", username, ToUser)
+	query := `
+	 SELECT fromUser, date, message, toUser 
+        FROM messages 
+        WHERE (fromUser = ? AND toUser = ?) OR (fromUser = ? AND toUser = ?) 
+        ORDER BY date ASC, id ASC
+	`
+	rows, err := db.Query(query, username, ToUser, ToUser, username)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var messages []structs.SMessage
+	for rows.Next() {
+		err := rows.Scan(&msg.From, &msg.Date, &msg.Message, &msg.To)
+		if err != nil {
+			return nil, err
+		}
+		msg.Type = "message"
+		messages = append(messages, msg)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+}
