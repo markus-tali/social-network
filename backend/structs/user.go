9ade8f4ce7fde8ede168fca25dd0f04fdc9b91aa
MikeNike <kooduser@pupil.kood.tech>
2024-10-23 11:52:08 +0300

    struct changes

--- backend/structs/user.go
+++ backend/structs/user.go
@@ -2,14 +2,16 @@ package structs
 
 type User struct {
 	Id          string
-	DateofBirth string `json:"dateofBirth"`
-	Username    string `json:"username"`
-	Password    string `json:"password"`
-	FirstName   string `json:"firstName"`
-	LastName    string `json:"lastName"`
-	Email       string `json:"email"`
-	AboutMe     string `json:"aboutMe"`
-	Nickname    string `json:"nickname"`
-	Avatar      string `json:"avatar"`
-	IsPrivate   bool   `json:"isPrivate"`
+	DateofBirth string   `json:"dateofBirth"`
+	Username    string   `json:"username"`
+	Password    string   `json:"password"`
+	FirstName   string   `json:"firstName"`
+	LastName    string   `json:"lastName"`
+	Email       string   `json:"email"`
+	AboutMe     string   `json:"aboutMe"`
+	Nickname    string   `json:"nickname"`
+	Avatar      string   `json:"avatar"`
+	IsPrivate   bool     `json:"isPrivate"`
+	FollowedBy  []string `json:"followedBy"`
+	IsFollowing []string `json:"isFollowing"`
 }
